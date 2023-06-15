ca9a19dfb76ce0932278ecc6e52ec38221d017b8
Ido Ben-Hur <idoybh2@gmail.com>
2023-06-15 11:58:42 +0300

    base: Allow configuring wake sensors on ambient [1/2]

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -138,6 +138,12 @@ public class AmbientDisplayConfiguration {
     }
 
     /** @hide */
+    public boolean pickupGestureOnAmbient(int user) {
+        return boolSettingDefaultOn(Settings.Secure.DOZE_PICK_UP_GESTURE_ALLOW_AMBIENT, user)
+                && pickupGestureEnabled(user) && pulseOnNotificationEnabled(user);
+    }
+
+    /** @hide */
     public boolean dozePickupSensorAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_dozePulsePickup);
     }
@@ -158,6 +164,12 @@ public class AmbientDisplayConfiguration {
     public boolean tapGestureVibrate(int user) {
         return boolSettingDefaultOff(Settings.Secure.DOZE_TAP_GESTURE_VIBRATE, user)
                 && tapGestureEnabled(user);
+    }
+
+    /** @hide */
+    public boolean tapGestureOnAmbient(int user) {
+        return boolSettingDefaultOn(Settings.Secure.DOZE_TAP_GESTURE_ALLOW_AMBIENT, user)
+                && tapGestureEnabled(user) && pulseOnNotificationEnabled(user);
     }
 
     /** @hide */
@@ -191,6 +203,12 @@ public class AmbientDisplayConfiguration {
     public boolean doubleTapGestureVibrate(int user) {
         return boolSettingDefaultOff(Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_VIBRATE, user)
                 && doubleTapGestureEnabled(user);
+    }
+
+    /** @hide */
+    public boolean doubleTapGestureOnAmbient(int user) {
+        return boolSettingDefaultOn(Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_ALLOW_AMBIENT, user)
+                && doubleTapGestureEnabled(user) && pulseOnNotificationEnabled(user);
     }
 
     /** @hide */
